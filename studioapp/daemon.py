5ce2cdd9d590d97966ade6d69aa6229b1f16d781
D1ms0n <studio7day@gmaile.com>
2017-03-19 23:39:09 +0200

    add test daemon

--- studioapp/daemon.py
+++ studioapp/daemon.py
@@ -2,8 +2,10 @@ from logger import Logger
 import time
 
 logger = Logger('DAEMON')
-
+print 'start'
 while 1==1:
+	print 'sleep'
 	time.sleep(10)
 	t = time.strftime('%X %x').replace(' ', '_').replace('/', '_')
+	print t
 	logger.log(t)	
