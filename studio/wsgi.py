7d9a5c791f4d4b9efca8e5ff35e6a541f059385f
D1ms0n <studio7day@gmaile.com>
2017-03-19 21:01:12 +0200

    try to change wsgi

--- studio/wsgi.py
+++ studio/wsgi.py
@@ -9,7 +9,24 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
+
+
 from django.core.wsgi import get_wsgi_application
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studio.settings")
 
