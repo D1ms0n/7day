7f6778959c03a87ad55a4c2b1f89ff47f48c91c7
Nambevan <mag_got@i.ua>
2017-04-18 23:31:24 +0300

    update

--- studioapp/static/scripts/contollers.js
+++ studioapp/static/scripts/contollers.js
@@ -40,7 +40,6 @@ angular.module('application')
             }).then(
                 function successCallback(response) {
                     // $scope.users = response.data;
-                    window.location = "/tasks";
                     preloader.style.display='none';
                 },
                 function errorCallback(response) {
@@ -167,7 +166,7 @@ angular.module('application')
         $scope.getTask = function (taskId) {
             console.log(taskId);
             localStorage.setItem('taskId',taskId);
-            window.location = "/task";
+            // window.location = "/#/task";
         };
 
         $scope.delTask = function (taskId) {
